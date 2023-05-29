3b365c129e736a8e5f4e86b1043551db4bfc2811
TeTedo <qkrxotjr9508@gmail.com>
2023-05-29 10:41:21 +0900

    Chore : add DI using requiredArgsConstructor annotation

--- src/main/java/com/delivery/toy/domain/food/controller/FoodController.java
+++ src/main/java/com/delivery/toy/domain/food/controller/FoodController.java
@@ -1,4 +1,11 @@
 package com.delivery.toy.domain.food.controller;
 
+import com.delivery.toy.domain.food.service.FoodService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
 public class FoodController {
+    private final FoodService foodService;
 }
