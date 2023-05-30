4bcfdefe79b227adb2ce21fff5eedc6ac2af20e6
TeTedo <qkrxotjr9508@gmail.com>
2023-05-30 22:08:40 +0900

    Feature : connect mariadb, create food entity

--- src/main/java/com/delivery/toy/domain/food/model/Food.java
+++ src/main/java/com/delivery/toy/domain/food/model/Food.java
@@ -1,14 +1,27 @@
 package com.delivery.toy.domain.food.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Food {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
     private Long id;
+    @Column(nullable = false, length = 100)
     private String name;
+    @Column(nullable = false)
     private double caloriePerGram;
+    @Column(nullable = false)
     private double carbohydratePerGram;
+    @Column(nullable = false)
     private double proteinPerGram;
+    @Column(nullable = false)
     private double provincePerGram;
+    @Column(nullable = false)
     private int grams;
+    @Column(nullable = false)
     private int price;
+    @Column(nullable = false, length = 255)
     private String imgPath;
 
 }
--- src/main/java/com/delivery/toy/domain/food/repository/FoodRepository.java
+++ src/main/java/com/delivery/toy/domain/food/repository/FoodRepository.java
@@ -1,7 +1,9 @@
 package com.delivery.toy.domain.food.repository;
 
+import com.delivery.toy.domain.food.model.Food;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class FoodRepository {
+public interface FoodRepository extends JpaRepository<Food,Long> {
 }
