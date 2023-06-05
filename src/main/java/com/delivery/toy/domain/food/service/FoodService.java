03dc6eee9f6b6b632c072f1bf0c5c0cd2e5ec030
TeTedo <qkrxotjr9508@gmail.com>
2023-06-06 00:48:26 +0900

    clean : clean imports

--- src/main/java/com/delivery/toy/domain/food/service/FoodService.java
+++ src/main/java/com/delivery/toy/domain/food/service/FoodService.java
@@ -2,14 +2,7 @@ package com.delivery.toy.domain.food.service;
 
 import com.delivery.toy.domain.food.dto.request.CreateFoodRequest;
 import com.delivery.toy.domain.food.dto.request.FindByFoodIdRequest;
-import com.delivery.toy.domain.food.exception.FoodIllegalException;
-import com.delivery.toy.domain.food.mapper.FoodMapper;
 import com.delivery.toy.domain.food.model.Food;
-import com.delivery.toy.domain.food.repository.FoodRepository;
-import lombok.RequiredArgsConstructor;
-
-import org.springframework.stereotype.Service;
-import org.springframework.util.Assert;
 
 public interface FoodService {
 
--- src/test/java/com/delivery/toy/domain/food/repository/FoodRepositoryTest.java
+++ src/test/java/com/delivery/toy/domain/food/repository/FoodRepositoryTest.java
@@ -6,7 +6,6 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
