7c0d2e0f738886ee673ab35bfc2f5d41bbbb6d61
TeTedo <qkrxotjr9508@gmail.com>
2023-06-06 00:43:07 +0900

    Feature (FoodRepositoryTest) : save, find test code

--- src/test/java/com/delivery/toy/domain/food/repository/FoodRepositoryTest.java
+++ src/test/java/com/delivery/toy/domain/food/repository/FoodRepositoryTest.java
@@ -1,8 +1,68 @@
 package com.delivery.toy.domain.food.repository;
 
+import com.delivery.toy.domain.food.exception.FoodIllegalException;
+import com.delivery.toy.domain.food.model.Food;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest
 public class FoodRepositoryTest {
 
+    @Autowired
+    FoodRepository foodRepository;
+
+    Food food;
+
+    @BeforeEach
+    void setUp(){
+        String name = "salad";
+        double caloriePerGram = 1.2;
+        double carbohydratePerGram = 0.03;
+        double proteinPerGram = 0.05;
+        double provincePerGram = 0.01;
+        int grams = 250;
+        int price = 12000;
+        String imgPath = "tempImgPath";
+
+        food = Food
+                .builder()
+                .name(name)
+                .caloriePerGram(caloriePerGram)
+                .carbohydratePerGram(carbohydratePerGram)
+                .proteinPerGram(proteinPerGram)
+                .provincePerGram(provincePerGram)
+                .grams(grams)
+                .price(price)
+                .imgPath(imgPath)
+                .build();
+    }
+    @DisplayName("Food 저장 테스트")
+    @Test
+    void save(){
+        // when
+        Food savedFood = foodRepository.save(food);
+
+        // then
+        Assertions.assertThat(savedFood).isEqualTo(food);
+    }
+
+    @DisplayName("Food 조회 테스트")
+    @Test
+    void find(){
+        // given
+        save();
+
+        // when
+        Food foundFood = foodRepository.findById(1L)
+                .orElseThrow(()-> new FoodIllegalException("해당 Id는 없는 Food입니다."));
+
+        // then
+        Assertions.assertThat(foundFood).isEqualTo(food);
+
+    }
 }
