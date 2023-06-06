04a2f223a939f7e7c7efd39b62b705a07c38056c
TeTedo <qkrxotjr9508@gmail.com>
2023-06-06 17:55:52 +0900

    Feature (Flyway) : add flyway

--- src/main/java/com/delivery/toy/domain/food/mapper/FoodMapper.java
+++ src/main/java/com/delivery/toy/domain/food/mapper/FoodMapper.java
@@ -3,7 +3,9 @@ package com.delivery.toy.domain.food.mapper;
 import com.delivery.toy.domain.food.dto.request.CreateFoodRequest;
 import com.delivery.toy.domain.food.dto.response.FoodResponse;
 import com.delivery.toy.domain.food.model.Food;
+import org.springframework.stereotype.Component;
 
+@Component
 public class FoodMapper {
 
     public Food toFood(CreateFoodRequest createFoodRequest) {
