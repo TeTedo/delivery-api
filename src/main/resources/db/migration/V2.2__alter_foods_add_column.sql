850abec30f96f0e1816f6d4650485e2d943a1461
TeTedo <qkrxotjr9508@gmail.com>
2023-06-08 01:23:13 +0900

    Chore (test) : separate test env

--- src/main/resources/db/migration/V2.2__alter_foods_add_column.sql
+++ src/main/resources/db/migration/V2.2__alter_foods_add_column.sql
@@ -1,3 +1,3 @@
 ALTER TABLE `foods`
-    ADD COLUMN  created_at  dateTime    NOT NULL    DEFAULT CURRENT_TIMESTAMP,
-    ADD COLUMN  updated_at  dateTime    NOT NULL    DEFAULT CURRENT_TIMESTAMP;
+    ADD COLUMN  created_at  dateTime    NOT NULL    DEFAULT now(),
+    ADD COLUMN  updated_at  dateTime    NOT NULL    DEFAULT now();
