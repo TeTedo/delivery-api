5615b9935b29157955ccfc8ba05692372a379736
TeTedo <qkrxotjr9508@gmail.com>
2023-06-20 22:30:24 +0900

    Refactor : change error handler arg

--- src/main/java/com/delivery/toy/domain/food/exception/FoodNotFoundException.java
+++ src/main/java/com/delivery/toy/domain/food/exception/FoodNotFoundException.java
@@ -1,7 +1,7 @@
 package com.delivery.toy.domain.food.exception;
 
 public class FoodNotFoundException extends IllegalArgumentException {
-    public FoodNotFoundException(String s) {
-        super(s);
+    public FoodNotFoundException(String msg) {
+        super(msg);
     }
 }
